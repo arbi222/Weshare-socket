a921b5a7f5b148b0334965772273b51e3594d925
Arbi <64432385+arbi222@users.noreply.github.com>
2025-01-18 23:29:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+const cron = require("cron");
+const https = require("https");
+
 const port = process.env.PORT || 8900;
 const io = require("socket.io")(port, {
     cors:{
         origin:["https://wesharemedia.onrender.com", "https://wesharemessenger.onrender.com"],
     },
 })
+
+// Adding a cron job to keep my server alive because the host (Render) on free usage will make my server inactive 
+// if no request is being made in 10-15 minutes.
+const backendUrl = "https://wesharesocketserver.onrender.com";
+const job = new cron.CronJob('*/8 * * * *', function(){
+  https.get(backendUrl, (res) => {
+    if (res.statusCode === 200){
+      console.log("server restarted")
+    }
+  }).on("error", (err) => {
+      console.log("error");
+  })
+})
+job.start();
 
 let users = []
 
